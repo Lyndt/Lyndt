b3630ad0e5fc5ab7d8cc106899eb277aa6e47c7c
Linda <m4rn0ta@gmail.com>
2021-11-24 13:50:53 +0100

    different values

---
+++ Week 1/Day 2/src/Values.java
@@ -0,0 +1,11 @@
+public class Values {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Lyndt!");
+        System.out.println(9);
+        System.out.println(9.999);
+
+    }
+
+
+}
