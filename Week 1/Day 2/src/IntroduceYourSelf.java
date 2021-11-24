3ea64c548965fd863209406bc24e4f33be57dbc8
Linda <m4rn0ta@gmail.com>
2021-11-24 14:03:36 +0100

    naposled jsem pridala osobni udaje

---
+++ Week 1/Day 2/src/IntroduceYourSelf.java
@@ -0,0 +1,17 @@
+public class IntroduceYourSelf {
+    public static void main(String[] args) {
+
+        System.out.println("Linda Sukovská");
+
+        System.out.println("34");
+
+        System.out.println("1.57");
+
+    }
+
+
+
+
+
+
+}
