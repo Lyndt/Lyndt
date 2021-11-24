768a04c21730670f89ce8facd5f1e64de3575d81
Linda <m4rn0ta@gmail.com>
2021-11-24 13:19:48 +0100

    dalsi den, prvni ukol

---
+++ src/Zaklady.java
@@ -0,0 +1,16 @@
+public class Zaklady {
+    public static void main(String[] args) {
+        //psvm - zkratka pro main
+
+        //single line komentar
+        /*
+        multiline komentar
+        dalsi linka
+
+         */
+//
+
+
+
+    }
+}
