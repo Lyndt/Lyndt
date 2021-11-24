1c28d98d36daa7a61180f09ba772ebfb18518ca5
Linda <m4rn0ta@gmail.com>
2021-11-24 15:45:55 +0100

    jeste se k tomuhle vratim

---
+++ Week 1/Day 2/src/TwoNumbers.java
@@ -0,0 +1,19 @@
+public class TwoNumbers {
+    public static void main(String[] args) {
+
+        double a = 22;
+        double b = 13;
+        System.out.println(13 + 22);
+        System.out.println(13 - 22);
+        System.out.println(22 * 13);
+        System.out.println(a / b);
+        System.out.println(22 / 13);
+        System.out.println(a % b);
+
+
+
+
+
+
+    }
+}
